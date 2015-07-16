a29d63c3037295019796f632dbd7eab17d344bbd
Shashank Shetti <shashank.shetti@gmail.com>
2015-07-15 23:11:59 -0700

    Adding Bower and mvc

--- src/PortlandTelangana/Startup.cs
+++ src/PortlandTelangana/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 
+
 namespace PortlandTelangana
 {
     public class Startup
@@ -13,14 +14,20 @@ namespace PortlandTelangana
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
+
+        //public void Configure(IApplicationBuilder app)
+        //{
+        //    app.Run(async (context) =>
+        //    {
+        //        await context.Response.WriteAsync("Hello World!");
+        //    });
+        //}
     }
 }
